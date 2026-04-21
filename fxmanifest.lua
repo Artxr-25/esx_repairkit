64461ce65ce03e53f6578f659e16c3e295cf3a2f
Artxr-25 <zwtzichartur@gmail.com>
2026-04-21 08:21:59 +0200

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,8 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fxversion 'cerulean'
 
+game 'gta5'
 description 'ESX Repairkit'
-version '1.0.3'
+version '1.1.0'
 
 client_scripts {
 	'@es_extended/locale.lua',
